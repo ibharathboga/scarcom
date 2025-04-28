32e7ab063da9f0d798292aca81517615dcb2fda0
ibharathboga <ibharathboga@gmail.com>
2025-04-28 16:28:31 +0530

    wip: db-message.js: include sender receiver info

--- src/lib/db-functions/db-message.js
+++ src/lib/db-functions/db-message.js
@@ -5,8 +5,33 @@ export async function createMessage(channelId, userId, message) {
 		.insert(schema.messages)
 		.values({ channelId, userId, message })
 		.returning();
-	return newMessage;
+
+	const participants = await db
+		.select({
+			userId: schema.users.id,
+			username: schema.users.username,
+			email: schema.users.email,
+		})
+		.from(schema.channelParticipants)
+		.innerJoin(schema.users, orm.eq(schema.channelParticipants.userId, schema.users.id))
+		.where(
+			orm.eq(schema.channelParticipants.channelId, channelId)
+		);
+
+	const sender = participants.find(p => p.userId === userId);
+	const receiver = participants.find(p => p.userId !== userId);
+
+	return {
+		newMessage,
+		newMessageInfo: {
+			senderName: sender.username,
+			senderEmail: sender.email,
+			receiverName: receiver.username,
+			receiverEmail: receiver.email,
+		},
+	}
 }
+
 
 export async function deleteMessage(messageId) {
 	await db.delete(schema.messages).where(orm.eq(schema.messages.id, messageId));
