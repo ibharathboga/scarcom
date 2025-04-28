4d51500926d60aee42c6a128c51c22ff4b451e57
ibharathboga <ibharathboga@gmail.com>
2025-04-28 15:18:22 +0530

    wip: useConversations hook

---
+++ src/hooks/useConversations.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import usePusherChannel from "./usePusherChannel";
+
+export default function useConversations() {
+    const session = useSession();
+    const [isLoading, setIsLoading] = useState(true);
+    const [list, setList] = useState([])
+
+    const { pusherRef } = usePusherChannel('conversations');
+
+    const pingConversations = async () => {
+        if (!session.data?.user?.id) return;
+        setIsLoading(prev => true)
+        const payload = { user_id: session.data.user.id };
+
+        try {
+            const response = await fetch("/api/conversations", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(payload),
+            });
+
+            console.log(response);
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                setList(data);
+            } else {
+                console.error("Failed to fetch conversations");
+            }
+        } catch (error) {
+            console.error("Error pinging conversations:", error);
+        }
+        setIsLoading(prev => false)
+    };
+
+    const updateConversationsList = (newMessageInfo) => {
+        console.log(newMessageInfo);
+    }
+
+    useEffect(() => {
+        if (session.status === "loading") return;
+        pingConversations();
+        pusherRef.bind("new_message", updateConversationsList);
+    }, [session.status]);
+
+    return {
+        isConversationsLoading: isLoading,
+        setIsConversationsLoading: setIsLoading,
+        word,
+        list,
+    };
+}
